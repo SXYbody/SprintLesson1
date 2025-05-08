05caebe61dbc374ae38766064f8548fcaf9f59bb
saveliy <saveliykislic88@mail.ru>
2025-05-08 23:28:58 +0700

    TS-2 commit

---
+++ src/main/kotlin/Lessons_1/task_2.kt
@@ -0,0 +1,19 @@
+package org.example.Lessons_1
+
+fun main() {
+
+    var checkList = 75
+
+    var coopList = 2000
+
+    val thankMessage = "Спасибо за покупку, оничан!"
+
+    println("Число покупателей: " + checkList)
+    println("Сообщение при покупке: " + thankMessage)
+    //println("Число сотрудников: " + coopList)
+
+    coopList = 2000 - 1
+
+    println("Число сотрудников: " + coopList)
+
+}
