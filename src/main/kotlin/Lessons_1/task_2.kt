e3ff6705e72ae67160270bce79fe52920a1369a0
saveliy <saveliykislic88@mail.ru>
2025-05-09 20:16:08 +0700

    KS-2 rework

--- src/main/kotlin/Lessons_1/task_2.kt
+++ src/main/kotlin/Lessons_1/task_2.kt
@@ -3,17 +3,15 @@ package org.example.Lessons_1
 fun main() {
 
     var checkList = 75
-
-    var coopList = 2000
-
     val thankMessage = "Спасибо за покупку, оничан!"
 
-    println("Число покупателей: " + checkList)
-    println("Сообщение при покупке: " + thankMessage)
-    //println("Число сотрудников: " + coopList)
+    println("Число покупателей: $checkList")
+    println("Сообщение при покупке: $thankMessage")
+
+    var coopList = 2000
+    //println("Число сотрудников: $coopList")
 
     coopList = 2000 - 1
-
-    println("Число сотрудников: " + coopList)
+    println("Число сотрудников: $coopList")
 
 }
