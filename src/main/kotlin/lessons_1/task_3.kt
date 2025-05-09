ce7bb129959bd99af705cae9abab8031f4de01ef
saveliy <saveliykislic88@mail.ru>
2025-05-09 20:49:32 +0700

    KS-3 first commit

---
+++ src/main/kotlin/lessons_1/task_3.kt
@@ -0,0 +1,17 @@
+package org.example.lessons_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("В $year Юрий Гагарин отправился в космос")
+    print("В $hour часов ")
+    println("$minute минут")
+
+    hour = 10
+    minute = 55
+
+    print("Время посадки - $hour:$minute")
+
+}
