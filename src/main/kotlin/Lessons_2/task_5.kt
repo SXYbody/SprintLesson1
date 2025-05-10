ba63e4700c643706b3743f0913b5b614ec934b93
saveliy <saveliykislic88@mail.ru>
2025-05-10 20:34:06 +0700

    KS-2-5

---
+++ src/main/kotlin/Lessons_2/task_5.kt
@@ -0,0 +1,13 @@
+package org.example.Lessons_2
+
+import kotlin.math.pow
+
+fun main() {
+
+    var deposit: Double = 70000.0
+
+    deposit = deposit*(1+ 16.7/100).pow(20)
+
+    println(deposit)
+
+}
