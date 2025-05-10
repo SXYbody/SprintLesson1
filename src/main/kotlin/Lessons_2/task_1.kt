7b7179f3f53a5dff671f0b9c9ddb0a01c93df7c8
saveliy <saveliykislic88@mail.ru>
2025-05-10 20:11:34 +0700

    KS-2-1 float ballFromEnglish

--- src/main/kotlin/Lessons_2/task_1.kt
+++ src/main/kotlin/Lessons_2/task_1.kt
@@ -1,8 +1,8 @@
-package org.example.Lessons_1
+package org.example.Lessons_2
 
 fun main() {
-    val childrenInClass: Float = 4.00f
-    val ballFromEnglish: Int = (3+4+3+5)
+    val childrenInClass = 4
+    val ballFromEnglish: Float = 3.0f + 4.0f + 3.0f + 5.0f
 
-    println(ballFromEnglish / childrenInClass)
+    println((ballFromEnglish / childrenInClass))
 }
