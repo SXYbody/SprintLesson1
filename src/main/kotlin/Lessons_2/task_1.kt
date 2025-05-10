8cd74527329a8bc76e3de34fea6b3719f9554239
saveliy <saveliykislic88@mail.ru>
2025-05-10 12:02:27 +0700

    KS-2-1 rework empty space

--- src/main/kotlin/Lessons_2/task_1.kt
+++ src/main/kotlin/Lessons_2/task_1.kt
@@ -1,11 +1,8 @@
 package org.example.Lessons_1
 
 fun main() {
-
     val childrenInClass: Float = 4.00f
     val ballFromEnglish: Int = (3+4+3+5)
 
     println(ballFromEnglish / childrenInClass)
-
-
 }
