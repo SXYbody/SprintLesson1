aa91fd671faac21feee8064905f51dbc4b1c53fb
saveliy <saveliykislic88@mail.ru>
2025-05-10 19:03:08 +0700

    KS-2-4

---
+++ src/main/kotlin/Lessons_2/task_4.kt
@@ -0,0 +1,13 @@
+package org.example.Lessons_2
+
+fun main() {
+
+    val ironOre = 11
+    val cristalOre = 7
+
+    println("Вы добыли $ironOre железной руды.")
+    println("+ " + (ironOre * 20) / 100 + " руды с помощью баффа")
+
+    println("Вы добыли $cristalOre кристаллической руды.")
+    println("+ " + (cristalOre * 20) / 100 + " руды с помощью баффа")
+}
