b861934c1e690b4b5b206909aeb05d337e9931c7
saveliy <saveliykislic88@mail.ru>
2025-05-10 11:49:19 +0700

    KS-2 rework checkList

--- src/main/kotlin/Lessons_1/task_2.kt
+++ src/main/kotlin/Lessons_1/task_2.kt
@@ -2,7 +2,7 @@ package org.example.Lessons_1
 
 fun main() {
 
-    var checkList = 75
+    val checkList = 75
     val thankMessage = "Спасибо за покупку, оничан!"
 
     println("Число покупателей: $checkList")
