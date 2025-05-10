dc57a0bdc48b9f7879e9e5cb117e2d20f92693e1
saveliy <saveliykislic88@mail.ru>
2025-05-10 11:56:25 +0700

    KS-3 rework string message

--- src/main/kotlin/lessons_1/task_3.kt
+++ src/main/kotlin/lessons_1/task_3.kt
@@ -1,16 +1,15 @@
 package org.example.lessons_1
 
 fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
 
-    println("В $year Юрий Гагарин отправился в космос")
-    print("В $hour часов ")
-    println("$minute минут")
+    print("В $year Юрий Гагарин отправился в космос ")
+    println("в $hour:$minute часов ")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     print("Время посадки - $hour:$minute")
 
