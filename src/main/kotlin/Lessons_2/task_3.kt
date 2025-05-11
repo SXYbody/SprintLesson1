a57716835f0d979ae0e48aa06dbd544a99340f04
saveliy <saveliykislic88@mail.ru>
2025-05-11 16:56:39 +0700

    KS-2-3 delete MagicNumber

--- src/main/kotlin/Lessons_2/task_3.kt
+++ src/main/kotlin/Lessons_2/task_3.kt
@@ -2,11 +2,15 @@ package org.example.Lessons_2
 
 fun main() {
 
-    val startTrain = 9 * 60 + 39
+    val minuteSplitHour = 60
+    val startTrainHour = 9
+    val startTrainMinute = 39
+
+    val startTrain = startTrainHour * minuteSplitHour + startTrainMinute
     val longRide = 457
     val endTrain = startTrain + longRide
-    val hour = endTrain / 60
-    val minute = endTrain % 60
+    val hour = endTrain / minuteSplitHour
+    val minute = endTrain % minuteSplitHour
 
     println("Поезд прибудет в [$hour:$minute]")
 }
