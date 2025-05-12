ee3e6a58f4f3c3636d0e5d652949598522b0ba39
saveliy <saveliykislic88@mail.ru>
2025-05-12 19:34:50 +0700

    KS-3-5 rename

--- src/main/kotlin/Lessons_2/task_3.kt
+++ src/main/kotlin/Lessons_2/task_3.kt
@@ -1,16 +1,16 @@
 package org.example.Lessons_2
 
- const val minuteSplitHour: Int = 60
+ const val MINUTE_IN_HOUR: Int = 60
 
 fun main() {
     val startTrainHour = 9
     val startTrainMinute = 39
 
-    val startTrain = startTrainHour * minuteSplitHour + startTrainMinute
+    val startTrain = startTrainHour * MINUTE_IN_HOUR + startTrainMinute
     val longRide = 457
     val endTrain = startTrain + longRide
-    val hour = endTrain / minuteSplitHour
-    val minute = endTrain % minuteSplitHour
+    val hour = endTrain / MINUTE_IN_HOUR
+    val minute = endTrain % MINUTE_IN_HOUR
 
     println("Поезд прибудет в [$hour:$minute]")
 }
