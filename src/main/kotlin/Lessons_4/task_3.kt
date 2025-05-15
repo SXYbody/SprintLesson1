d1cc05125c50850753a4fc2efb42727629e18b10
saveliy <saveliykislic88@mail.ru>
2025-05-15 11:44:04 +0700

    KS-4-3

--- src/main/kotlin/Lessons_4/task_3.kt
+++ src/main/kotlin/Lessons_4/task_3.kt
@@ -1,23 +1,21 @@
 package org.example.Lessons_4
 
-const val SUNNY_WEATHER: Boolean = true
-const val OPEN_TENT: Boolean = true
-const val HUMIDITY: Int = 20
-const val TIME_OF_YEAR: String = "Лето, Весна, Осень"
+const val isSunny: Boolean = true
+const val isTentOpen: Boolean = true
+const val isHumidity: Int = 20
+const val isNeverTime: String = "Зима"
 
 fun main() {
 
-    val todaySunnyWeather: Boolean = true
-    val todayOpenTent: Boolean = true
-    val todayHumidity: Int = 20
-    val todayTimeOfYear = "Зима"
+    val isSunnyToday: Boolean = true
+    val isTentOpenToday: Boolean = true
+    val isHumidityToday: Int = 20
+    val isNeverTimeToday = "Зима"
 
-    val ability: Boolean = (SUNNY_WEATHER == todaySunnyWeather)
-            && (OPEN_TENT == todayOpenTent)
-            && (HUMIDITY == todayHumidity)
-            && (TIME_OF_YEAR == todayTimeOfYear)
+    val ability: Boolean = (isSunny == isSunnyToday)
+            && (isTentOpen == isTentOpenToday)
+            && (isHumidity == isHumidityToday)
+            && (isNeverTime != isNeverTimeToday)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $ability")
-
-
 }
