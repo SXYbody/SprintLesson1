7039ade86fe051b4c13fb294934c68a5be6bed19
saveliy <saveliykislic88@mail.ru>
2025-05-16 19:25:15 +0700

    KS-4-5 rename/readln

--- src/main/kotlin/Lessons_4/task_5.kt
+++ src/main/kotlin/Lessons_4/task_5.kt
@@ -3,31 +3,31 @@ package org.example.Lessons_4
 const val IS_DAMAGE = false
 const val MIN_CREW = 55
 const val MAX_CREW = 70
-const val GET_PROVISIONS = 50
-const val NICE_WEATHER = true
+const val MIN_PROVISIONS = 50
+const val IS_WEATHER_GOOD = true
 
 fun main() {
 
-    val isDamage: Boolean = true
-    val getCrew: Int = 60
-    val getProvisions: Int = 60
-    val niceWeather: Boolean = false
+    val isDamage: Boolean = readln().toBoolean()
+    val getCrew: Int = readln().toInt()
+    val getProvisions: Int = readln().toInt()
+    val isWeatherGood: Boolean = readln().toBoolean()
 
     val result1 = (isDamage == IS_DAMAGE) &&
             ((getCrew >= MIN_CREW) && (getCrew < MAX_CREW)) &&
-            (getProvisions > GET_PROVISIONS)
+            (getProvisions > MIN_PROVISIONS)
 
     val result2 = (isDamage != IS_DAMAGE) &&
-            (getCrew == 70) &&
-            (getProvisions > GET_PROVISIONS) &&
-            (niceWeather == NICE_WEATHER)
+            (getCrew == MAX_CREW) &&
+            (getProvisions >= MIN_PROVISIONS) &&
+            (isWeatherGood == IS_WEATHER_GOOD)
 
     val generalResult = """
         Повреждений корабля:  $isDamage
         Количество персонала: $getCrew
         Количество провизии:  $getProvisions
         Хорошие погодные
-        условия:              $niceWeather
+        условия:              $isWeatherGood
         Можно выдвигатся:     ${result1 || result2}
     """.trimIndent()
 
