024875c02a182a609ccbf45f5d6d2cacc61d31b8
saveliy <saveliykislic88@mail.ru>
2025-05-16 19:32:48 +0700

    KS-5-2 const val/rename

--- src/main/kotlin/Lessons_5/task_2.kt
+++ src/main/kotlin/Lessons_5/task_2.kt
@@ -1,10 +1,13 @@
 package org.example.Lessons_5
 
+const val NOW_YEAR = 2025
+const val NEED_YEAR = 18
+
 fun main() {
     println("Введите год своего рождения")
-    val userAge = readln().toInt()
-    val userOld = 2025 - userAge
+    val userYear = readln().toInt()
+    val userOld = NOW_YEAR - userYear
 
-    if (userOld >= 18) println("Показать экран со скрытым контентом")
+    if (userOld >= NEED_YEAR) println("Показать экран со скрытым контентом")
     else println("Контент заблокирован.")
 }
