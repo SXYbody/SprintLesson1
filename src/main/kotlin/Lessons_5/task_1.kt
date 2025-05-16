ee6ddaf82db5ceb9262d8cf5be02c5ff5070ffae
saveliy <saveliykislic88@mail.ru>
2025-05-16 11:43:11 +0700

    KS-5-1

---
+++ src/main/kotlin/Lessons_5/task_1.kt
@@ -0,0 +1,10 @@
+package org.example.Lessons_5
+
+fun main() {
+
+    println("Решите пример: 6 + 11")
+    val userString = readln().toInt()
+
+    if (userString == 17) println("Добро пожаловать!")
+    else println("Доступ запрещен.")
+}
