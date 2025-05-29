cc039d5c6fabb8219db6be0442a73f796721a758
saveliy <saveliykislic88@mail.ru>
2025-05-30 00:31:55 +0700

    KS-5-3 formating

--- src/main/kotlin/Lessons_5/task_3.kt
+++ src/main/kotlin/Lessons_5/task_3.kt
@@ -2,7 +2,7 @@ package org.example.Lessons_5
 
 fun main() {
 
-    println("Для победы напишите любое число от 0 до 42")
+    println("Для победы напишите любые 2 числа от 0 до 42")
 
     val userNumber1 = readln().toInt()
     val userNumber2 = readln().toInt()
@@ -10,15 +10,15 @@ fun main() {
     val winNumber2 = (0..42).random()
     var guessesNumber: Int = 0
 
-    if ((userNumber1 == winNumber1)||(userNumber1 == winNumber2)) guessesNumber ++
+    if ((userNumber1 == winNumber1) || (userNumber1 == winNumber2)) guessesNumber++
 
-    if((userNumber2 == winNumber1)||(userNumber2 == winNumber2)) guessesNumber++
+    if ((userNumber2 == winNumber1) || (userNumber2 == winNumber2)) guessesNumber++
 
-    if (guessesNumber == 2){
+    if (guessesNumber == 2) {
         println("Поздравляем! Вы выиграли главный приз")
-    }else if(guessesNumber == 1){
+    } else if (guessesNumber == 1) {
         println("Вы выиграли утешительный приз!")
-    }else {
+    } else {
         println("Неудача!")
     }
 }
