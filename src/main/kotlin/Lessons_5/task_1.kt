3cab6454a40854f94a9d971b41dd6e91df32fd3c
saveliy <saveliykislic88@mail.ru>
2025-05-29 16:33:43 +0700

    KS-5-1 smth

--- src/main/kotlin/Lessons_5/task_1.kt
+++ src/main/kotlin/Lessons_5/task_1.kt
@@ -2,12 +2,12 @@ package org.example.Lessons_5
 
 fun main() {
 
-    val number1 = 6
-    val number2 = 11
+    val number1 = (0..10).random()
+    val number2 = (0..10).random()
 
     println("Решите пример: $number1 + $number2")
     val userString = readln().toInt()
 
-    if (userString == 17) println("Добро пожаловать!")
+    if (userString == number1 + number2) println("Добро пожаловать!")
     else println("Доступ запрещен.")
 }
