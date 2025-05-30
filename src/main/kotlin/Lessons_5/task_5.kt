5b9bb65e366d1be68250d62e872818c2ddc804e2
saveliy <saveliykislic88@mail.ru>
2025-05-30 11:56:21 +0700

    KS-5-5 when

--- src/main/kotlin/Lessons_5/task_5.kt
+++ src/main/kotlin/Lessons_5/task_5.kt
@@ -2,15 +2,18 @@ package org.example.Lessons_5
 
 fun main() {
 
+    println("Введите 3 часла, чтобы выйграть приз!")
+
     val winNumber = listOf((0..42).random(), (0..42).random(),(0..42).random())
     val userNumberList = listOf(readln().toInt(),readln().toInt(),readln().toInt())
     val guessNumber = winNumber.intersect(userNumberList).size
 
-    if (guessNumber == 3) println("Вы сорвали джекпот!!")
-    else if (guessNumber == 2) println("Вы выйграли крупный приз!")
-    else if (guessNumber == 1) println("Вы выйграли утешительный приз")
-    else println("К сожелению вы проиграли :(")
+    when(guessNumber){
+        3 -> println("Вы сорвали джекпот!!")
+        2 -> println("Вы выйграли крупный приз!")
+        1 -> println("Вы выйграли утешительный приз")
+        else -> println("К сожелению вы проиграли :(")
+    }
 
     println("Числа, которые нужно было угадать $winNumber")
-
 }
