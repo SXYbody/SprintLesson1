5c531585c482e5c756f8b54625a02f84703489ab
saveliy <saveliykislic88@mail.ru>
2025-05-30 12:07:54 +0700

    KS-5-6

--- src/main/kotlin/Lessons_5/task_6.kt
+++ src/main/kotlin/Lessons_5/task_6.kt
@@ -2,20 +2,25 @@ package org.example.Lessons_5
 
 import kotlin.math.pow
 
+const val centimeterFromMeter = 100
+
+const val firstHurdle = 15.5
+const val secondHurdle = 25
+const val threeHurdle = 30
+
 fun main() {
     println("Введите ваш вес(кг) / рост(см)")
 
     val weight: Double = readln().toDouble()
     var growth: Double = readln().toDouble()
-    val centimeterFromMeter = 100
 
     growth = growth / centimeterFromMeter
     val result = weight / growth.pow(2)
 
-    println("Ваш ИМТ: ${String.format("%.2f", result)}")
+    println("Ваш ИМТ: %.2f".format(result))
 
-    if (result < 18.5) println("Недостаточная масса тела")
-    else if ((18.5 <= result) && (result < 25)) println("Нормальная масса тела")
-    else if ((25 <= result) && (result < 30)) println("Избыточная масса тела")
-    else if (result > 30) println("Вы жирный!")
+    if (result < firstHurdle) println("Недостаточная масса тела")
+    else if ((firstHurdle <= result) && (result < secondHurdle)) println("Нормальная масса тела")
+    else if ((secondHurdle <= result) && (result < threeHurdle)) println("Избыточная масса тела")
+    else if (result > threeHurdle) println("Вы жирный!")
 }
