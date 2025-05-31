df75953b7b43c3edb46c49bc2529c6f1abdfa2dc
saveliy <saveliykislic88@mail.ru>
2025-05-31 12:56:24 +0700

    KS-5-7 reform

--- src/main/kotlin/Lessons_5/task_7.kt
+++ src/main/kotlin/Lessons_5/task_7.kt
@@ -1,17 +1,16 @@
 package org.example.Lessons_5
 
-const val fuelFromDistance = 100
+const val FUEL_PER_100KM = 100
 
 fun main() {
     println("Пожалуйста напиши по порядку: \nДистанция(км) \nРасход топлива(на 100 км) \nЦена за литр топлива")
 
-    val distance: Float = readln().toFloat()
-    val expenseFuel: Float = readln().toFloat()
-    val priceFuel: Float = readln().toFloat()
+    val distance: Double = readln().toDouble()
+    val expenseFuel: Double = readln().toDouble()
+    val priceFuel: Double = readln().toDouble()
 
-    val fuel = (distance * expenseFuel) / fuelFromDistance
+    val fuel = (distance * expenseFuel) / FUEL_PER_100KM
     val price = priceFuel * fuel
 
-    println("Необходимое кол-во топлива для поездки %.2f".format(fuel) +
-            "\nСтоимость поездки: %.2f".format(price))
+    println("Необходимое кол-во топлива для поездки %.2f \nСтоимость поездки: %.2f".format(fuel,price))
 }
