2c8df6608a9636b327f9bcde88568608a4af05e3
saveliy <saveliykislic88@mail.ru>
2025-05-31 22:28:06 +0700

    KS-5-6 when

--- src/main/kotlin/Lessons_5/task_6.kt
+++ src/main/kotlin/Lessons_5/task_6.kt
@@ -2,25 +2,26 @@ package org.example.Lessons_5
 
 import kotlin.math.pow
 
-const val centimeterFromMeter = 100
+const val CENTIMETER_METER = 100
 
-const val firstHurdle = 15.5
-const val secondHurdle = 25
-const val threeHurdle = 30
+const val FIRST_HURDLE = 15.5
+const val SECOND_HURDLE = 25
+const val THREE_HURDLE = 30
 
 fun main() {
     println("Введите ваш вес(кг) / рост(см)")
 
     val weight: Double = readln().toDouble()
-    var growth: Double = readln().toDouble()
+    var growth: Double = readln().toDouble() / CENTIMETER_METER
 
-    growth = growth / centimeterFromMeter
     val result = weight / growth.pow(2)
 
     println("Ваш ИМТ: %.2f".format(result))
 
-    if (result < firstHurdle) println("Недостаточная масса тела")
-    else if ((firstHurdle <= result) && (result < secondHurdle)) println("Нормальная масса тела")
-    else if ((secondHurdle <= result) && (result < threeHurdle)) println("Избыточная масса тела")
-    else if (result > threeHurdle) println("Вы жирный!")
+    when {
+        result < FIRST_HURDLE -> println("Недостаточная масса тела")
+        (FIRST_HURDLE <= result) && (result < SECOND_HURDLE) -> println("Нормальная масса тела")
+        (SECOND_HURDLE <= result) && (result < THREE_HURDLE) -> println("Избыточная масса тела")
+        else -> println("Вы жирный!")
+    }
 }
