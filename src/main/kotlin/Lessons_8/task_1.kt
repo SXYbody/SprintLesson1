93aa028fb0456311d31365f2f87a44b75cee7028
saveliy <saveliykislic88@mail.ru>
2025-06-07 18:18:31 +0700

    KS-8-1 sum

--- src/main/kotlin/Lessons_8/task_1.kt
+++ src/main/kotlin/Lessons_8/task_1.kt
@@ -2,12 +2,7 @@ package org.example.Lessons_8
 
 fun main() {
 
-    val reviewArray = arrayOf(100, 200, 150, 1000, 630, 134, 456)
-    var sumReviewArray = 0
+    val reviewArray = intArrayOf(100, 200, 150, 1000, 630, 134, 456)
 
-    for (i in 0 until reviewArray.size) {
-        sumReviewArray += reviewArray[i]
-    }
-
-    println("Количество просмотров: $sumReviewArray")
+    println("Количество просмотров: ${reviewArray.sum()}")
 }
