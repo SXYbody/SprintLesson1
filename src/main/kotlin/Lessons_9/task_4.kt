f1c366e44df962acf67358018f3b3531d3063410
saveliy <saveliykislic88@mail.ru>
2025-06-08 21:01:03 +0700

    KS-9-4 smth

--- src/main/kotlin/Lessons_9/task_4.kt
+++ src/main/kotlin/Lessons_9/task_4.kt
@@ -3,10 +3,8 @@ package org.example.Lessons_9
 fun main() {
     println("Введите черех запятую с пробелом продукты, которые хотите внести в список:")
 
-    val userList = readln().split(", ")
-    val ingredientsList = userList
+    val ingredientsList = readln().split(", ")
+    val sortedIngredientsList = ingredientsList.sorted()
 
-    ingredientsList.sorted()
-
-    println(ingredientsList)
+    println(sortedIngredientsList)
 }
