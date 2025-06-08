25bd4f40518fa2b68bf2af05e73dc00177cfce03
saveliy <saveliykislic88@mail.ru>
2025-06-08 19:50:54 +0700

    KS-9-2 smth

--- src/main/kotlin/Lessons_9/task_2.kt
+++ src/main/kotlin/Lessons_9/task_2.kt
@@ -2,20 +2,17 @@ package org.example.Lessons_9
 
 fun main() {
 
-    val ingredientsList = mutableListOf("Лук","Макароны","Помидор")
+    val ingredientsList = mutableListOf("Лук", "Макароны", "Помидор")
 
     println("Базовые продукты в рецепте: $ingredientsList")
     println("Желаете добавить еще?")
 
-    val userAnswer = readln().toString()
+    val userAnswer = readln()
 
-    if (userAnswer.equals("да", true)){
+    if (userAnswer.equals("да", ignoreCase = true)) {
         println("Какой ингредиент вы хотите добавить?")
-        val userIngredient = readln().toString()
+        val userIngredient = readln()
         ingredientsList.add(userIngredient)
         println("Теперь в рецепте есть следующие ингредиенты: $ingredientsList")
-
-    } else {
-        return
     }
 }
