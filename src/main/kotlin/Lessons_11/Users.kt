ca3aa90e87dd5ea3efdb9ba66c6775ff79459a79
saveliy <saveliykislic88@mail.ru>
2025-06-15 12:26:31 +0700

    KS-11-1

---
+++ src/main/kotlin/Lessons_11/Users.kt
@@ -0,0 +1,12 @@
+package org.example.Lessons_11
+
+class Users (
+    val id: Int,
+    val login: String,
+    val password: String,
+    val mail: String,
+){
+    fun getStatisticUser(){
+        println("ID: $id, Логин: $login, Пароль: $password, Почта: $mail")
+    }
+}
