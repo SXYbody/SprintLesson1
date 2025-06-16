42b999b8b5e5009518bc0aa85f0c992ee3598dfc
saveliy <saveliykislic88@mail.ru>
2025-06-17 00:45:26 +0700

    KS-11-4 smth

--- src/main/kotlin/Lessons_11_4/task_4.kt
+++ src/main/kotlin/Lessons_11_4/task_4.kt
@@ -1,55 +1,26 @@
 package org.example.Lessons_11_4
 
-class Dish(
-    val title: String,
-    val category: Category,
-    val recipe: Recipe,
-    val ingredients: Ingredient,
-) {
-    fun printDishInfo() {
-        println(
-            "Название блюда: $title, \nКатегория блюда: ${category.title}, " +
-                    "\nРецепт блюда: ${recipe.description}, \nИнгредиенты для блюда: ${ingredients.amount} ${ingredients.listOfIngredient}"
-        )
-    }
-}
-
 class Category(
     val title: String,
     val description: String,
-    val favorite: Boolean = false,
-) {
-    fun printCategoryInfo() {}
-}
+    val id: Int,
+    val imageUrl: String,
+) {}
 
 class Recipe(
     val title: String,
     val description: String? = null,
-    val amount: Int = 1,
-) {
-    fun printRecipeInfo() {}
-}
+    val id: Int,
+    val imageUrl: String,
+    val cookingMethod: List<String> = listOf(),
+) {}
 
 class Ingredient(
-    val listOfIngredient: List<String> = listOf(),
-    val amount: List<Int> = listOf()
-) {
-    fun printIngredientInfo() {}
-}
+    val title: String,
+    val amount: Int,
+    val unit: String,
+) {}
 
 fun main() {
-    val category1 = Category("Бургеры","Рецепты всех популярных видов бургеров")
-    val category2 = Category("Десерты","Самые вкусные рецепты десертов специально для вас")
-    val category3 = Category("Пицца","Пицца на любой вкус и цвет.Лучшая подборка для тебя")
-    val category4 = Category("Рыба","Печеная, жареная, сушеная, любая рыба на твой вкус")
 
-    val recipe1 = Recipe("Классический бургер")
-    val recipe2 = Recipe("Чизбургер")
-    val recipe3 = Recipe("Бургер с грибами и сыром")
-
-    val ingredients1 = Ingredient()
-
-    val dish1 = Dish("Бургер с грибами и сыром",category1,recipe3, ingredients1)
-
-    dish1.printDishInfo()
 }
