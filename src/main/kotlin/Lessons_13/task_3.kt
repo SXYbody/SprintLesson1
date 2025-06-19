507d9cadef2d9822fb14160bb283c6757c24ef0a
saveliy <saveliykislic88@mail.ru>
2025-06-19 23:08:02 +0700

    KS-13-3 smth

--- src/main/kotlin/Lessons_13/task_3.kt
+++ src/main/kotlin/Lessons_13/task_3.kt
@@ -1,25 +1,27 @@
 package org.example.Lessons_13
 
-val usersList: MutableList<UsersInformation2> = mutableListOf()
-
 class UsersInformation2(
-    val userName: String,
-    val userPhone: Long,
-    val userCompany: String? = null,
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
 ) {
     fun printUserInfo() {
-        println("Имя: $userName \nНомер: $userPhone \nКомпания: ${userCompany ?: "Не указано"}")
+        println("Компания: ${company ?: "Не указано"}")
     }
 }
 
 fun main() {
-    usersList.add(UsersInformation2("Уолтер", 79628456233, null))
-    usersList.add(UsersInformation2("Смитт", 79628456234, null))
+    val usersList: MutableList<UsersInformation2> = mutableListOf()
+
+    usersList.add(UsersInformation2("Уолтер", 79628456233))
+    usersList.add(UsersInformation2("Смитт", 79628456234))
     usersList.add(UsersInformation2("Андрей", 79628456235, "Reddit"))
     usersList.add(UsersInformation2("Шерлок", 79628456236, "Sony"))
     usersList.add(UsersInformation2("Кеша", 79628456231, "null"))
 
-    usersList.forEach {
-        it.printUserInfo()
+    usersList.map {
+        if (it.company != null){
+            println(it.company)
+        }
     }
 }
