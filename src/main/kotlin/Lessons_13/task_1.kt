566bad40de2f3b2dd6e4a4e05777949b3b12f33c
saveliy <saveliykislic88@mail.ru>
2025-06-19 13:32:54 +0700

    KS-13-1

---
+++ src/main/kotlin/Lessons_13/task_1.kt
@@ -0,0 +1,11 @@
+package org.example.Lessons_13
+
+class UsersInformation(
+    val userName: String,
+    val userPhone: Long,
+    val userCompany: String?,
+)
+
+fun main() {
+    UsersInformation("Уолтер",79628456235,null)
+}
