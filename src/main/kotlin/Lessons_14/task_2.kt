49ac419cd717caa97da1e2b2357a7a86600f74ad
saveliy <saveliykislic88@mail.ru>
2025-06-24 11:30:15 +0700

    KS-14-2 rename

--- src/main/kotlin/Lessons_14/task_2.kt
+++ src/main/kotlin/Lessons_14/task_2.kt
@@ -6,13 +6,11 @@ open class Ship2(
     val payLoad: Int = 150,
     val roominess: Int = 50,
 ) {
-    fun printShipInfo() {
+    fun displayInfo() {
         println("\nНазвание корабля: $name \nСкорость: $speed \nГрузоподъемность: $payLoad \nВместительность:$roominess")
     }
 
-    open fun singularProperty() {}
-
-    open fun singularLoad() {
+    open fun load() {
         println("Уникальных способов погрузки нету!")
     }
 }
@@ -22,7 +20,7 @@ class Liner1(
     roominess: Int = 150,
 ) : Ship2(name = name, roominess = roominess) {
 
-    override fun singularLoad() {
+    override fun load() {
         println("Выдвинулся горизонтальный трап со шкафаута")
     }
 }
@@ -33,7 +31,7 @@ class Cargo1(
     payLoad: Int = 200,
 ) : Ship2(name = name, speed = speed, payLoad = payLoad) {
 
-    override fun singularLoad() {
+    override fun load() {
         println("Активировалось погрузочные краны")
     }
 }
@@ -44,31 +42,31 @@ class Icebreaker1(
     roominess: Int = 25
 ) : Ship2(name = name, speed = speed, roominess = roominess) {
 
-    override fun singularProperty() {
+    fun singularProperty() {
         println("Корабль $name, обладает уникальной способность - колоть лёд на своем пути!")
     }
 
-    override fun singularLoad() {
+    override fun load() {
         println("Ворота со стороны кормы открыты")
     }
 }
 
 fun main() {
     val ship1 = Ship2("Просто корабль")
-    ship1.printShipInfo()
-    ship1.singularLoad()
+    ship1.displayInfo()
+    ship1.load()
 
     val liner1 = Liner1("Лайнер1")
-    liner1.printShipInfo()
-    liner1.singularLoad()
+    liner1.displayInfo()
+    liner1.load()
 
     val cargo1 = Cargo1("Карго1")
-    cargo1.printShipInfo()
-    cargo1.singularLoad()
+    cargo1.displayInfo()
+    cargo1.load()
 
     val iceBreak1 = Icebreaker1("Ледокол1")
-    iceBreak1.printShipInfo()
+    iceBreak1.displayInfo()
     iceBreak1.singularProperty()
-    iceBreak1.singularLoad()
+    iceBreak1.load()
 
 }
