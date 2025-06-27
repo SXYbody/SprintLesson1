d34dbc7692fdd5dd7e932f28c60ab318e643cc72
saveliy <saveliykislic88@mail.ru>
2025-06-27 18:55:34 +0700

    KS-14-5 smth

--- src/main/kotlin/Lessons_14/task_5.kt
+++ src/main/kotlin/Lessons_14/task_5.kt
@@ -4,22 +4,22 @@ class Chat() {
     val messageList = mutableListOf<Message>()
     var id = 0
 
-    fun addMessage(message: String): Message {
-        val message = Message(message, ++id, mutableListOf())
+    fun addMessage(message: String, user: User): Message {
+        val message = Message(message, ++id, mutableListOf(), user)
         messageList.add(message)
         return message
     }
 
-    fun addThreadMessage(idM: Message, littleMessage: String) {
-        idM.thread.add(LittleMessage(littleMessage, ++id))
+    fun addThreadMessage(parentMessage: Message, littleMessage: String, user: User) {
+        parentMessage.thread.add(LittleMessage(littleMessage, ++id, user))
     }
 
     fun printChat() {
         messageList.forEach { mes ->
-            println("${mes.message}")
+            println("${mes.userParent.name}: ${mes.message}")
             if (mes.thread.size > 0) {
                 mes.thread.forEach { litMes ->
-                    println(" ${litMes.message}")
+                    println("\t${litMes.userParent.name}: ${litMes.message}")
                 }
             }
         }
@@ -30,22 +30,31 @@ class Message(
     val message: String,
     val id: Int,
     val thread: MutableList<LittleMessage>,
+    val userParent: User,
 )
 
 class LittleMessage(
     val message: String,
     val id: Int,
+    val userParent: User
+)
+
+class User(
+    val name: String
 )
 
 fun main() {
 
-    val mes1 = Chat().addMessage("Привет")
-    Chat().addThreadMessage(mes1, "Привет, друг!")
-    Chat().addThreadMessage(mes1, "Иди отсюда!!")
-    Chat().addThreadMessage(mes1, "Я первый, можно медальку?")
+    val chat1 = Chat()
 
-    val mes2 = Chat().addMessage("Как у кого дела?")
-    Chat().addThreadMessage(mes2, "У меня не очень :(")
+    val mes1 = chat1.addMessage("Привет", User("Джон"))
+    chat1.addThreadMessage(mes1, "Привет, друг!", User("Nintendo"))
+    chat1.addThreadMessage(mes1, "Иди отсюда!!", User("Kiborg777"))
+    chat1.addThreadMessage(mes1, "Я первый, можно медальку??", User("Amogus"))
 
-    Chat().printChat()
+    val mes2 = chat1.addMessage("Как у кого дела?", User("Джон"))
+    chat1.addThreadMessage(mes2, "Не очень, с завода уволили:(", User("Кейл"))
+
+    chat1.printChat()
+
 }
