0c65b001c1d61620575fc7c1af2d3e4d9b09fae6
saveliy <saveliykislic88@mail.ru>
2025-07-01 16:34:32 +0700

    KS-15-3

---
+++ src/main/kotlin/Lessons_15/task_3.kt
@@ -0,0 +1,52 @@
+package org.example.Lessons_15
+
+val messageList = mutableListOf<String>()
+val userList = mutableListOf<CommonUser>()
+
+abstract class User(
+    val name: String,
+) : Addition {
+    override fun addMessage(message: String) {
+        messageList.add(message)
+    }
+}
+
+class CommonUser(
+    name: String,
+) : User(name = name)
+
+class Administrator(
+    name: String,
+) : User(name = name), Removal {
+
+    override fun deleteMessage(message: String) {
+        messageList.remove(message)
+    }
+
+    override fun deleteUser(user: CommonUser) {
+        userList.remove(user)
+    }
+
+}
+
+interface Addition {
+    fun addMessage(message: String)
+}
+
+interface Removal {
+    fun deleteMessage(message: String)
+    fun deleteUser(user: CommonUser)
+}
+
+fun main() {
+    val user1 = CommonUser("BobSTie")
+    val admin1 = Administrator("Кластер")
+    userList.add(user1)
+    val mes1 = user1.addMessage("Всем привет")
+
+    admin1.addMessage("Пока")
+    admin1.deleteUser(user1)
+    admin1.deleteMessage(mes1.toString())
+
+
+}
