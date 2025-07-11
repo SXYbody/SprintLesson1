6520fc26b04857d92216afe2c93a1bd29b275c7d
saveliy <saveliykislic88@mail.ru>
2025-07-11 22:41:40 +0700

    KS-17-4

---
+++ src/main/kotlin/Lessons_17/task_4.kt
@@ -0,0 +1,22 @@
+package org.example.Lessons_17
+
+class Premiss(
+    _number: String,
+    _location: String,
+){
+    val number = _number
+
+    var location = _location
+       set(value) {
+            field = value
+            locationQuantity += 1
+        }
+    var locationQuantity = 0
+}
+
+fun main() {
+    val premiss1 = Premiss("123","Таджикистан")
+    premiss1.location = "Россия"
+    premiss1.location = "Беларусь"
+    println(premiss1.locationQuantity)
+}
