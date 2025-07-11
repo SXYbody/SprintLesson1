82397c6733b94f6360893d45b3ce0aa90a187186
saveliy <saveliykislic88@mail.ru>
2025-07-11 23:38:16 +0700

    KS-17-5

---
+++ src/main/kotlin/Lessons_17/task_5.kt
@@ -0,0 +1,29 @@
+package org.example.Lessons_17
+
+class User(
+    _login: String,
+    _password: String,
+) {
+    var login = _login
+        set(value) {
+            field = value
+            println("Логин успешно сменён!")
+        }
+
+    var password = _password
+        get() {
+            var secretPassword = ""
+            for (i in field) secretPassword += "*"
+            return secretPassword
+        }
+        set(value) = println("Вы не можете изменить пароль!")
+}
+
+fun main() {
+    val user1 = User("Джон", "123")
+    user1.login = "Бэйбик"
+    println(user1.password)
+    user1.password = "01234785"
+
+
+}
