e6aa7074278fa0b823598a1c3bcfacaee2657f77
saveliy <saveliykislic88@mail.ru>
2025-07-18 10:18:23 +0700

    KS-19-1 add fishName

--- src/main/kotlin/Lessons_19/task_1.kt
+++ src/main/kotlin/Lessons_19/task_1.kt
@@ -1,13 +1,15 @@
 package org.example.Lessons_19
 
-enum class Fish() {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fish(
+    var fishName: String,
+) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
     println("Вы можете выбрать рыбку..")
-    Fish.values().forEach { println(it.name) }
+    Fish.values().forEach { println(it.fishName) }
 }
