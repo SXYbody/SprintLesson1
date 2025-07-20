dbf1da90940f7de0185a753d6e565a2721854b24
saveliy <saveliykislic88@mail.ru>
2025-07-20 21:20:54 +0700

    KS-19-5 smth

--- src/main/kotlin/Lessons_19/task_5.kt
+++ src/main/kotlin/Lessons_19/task_5.kt
@@ -5,16 +5,14 @@ class User4(
     val sex: Sex,
 )
 
-enum class Sex() {
+enum class Sex {
     MAN,
-    WOMAN,
-    NOT_SPECIFIED;
+    WOMAN;
 
     fun getSexString(): String {
         when (this) {
             MAN -> return "Мужчина"
             WOMAN -> return "Женщина"
-            NOT_SPECIFIED -> return "Не указано"
         }
     }
 }
@@ -22,24 +20,21 @@ enum class Sex() {
 fun main() {
     val list1 = mutableListOf<User4>()
 
-    println("Формат заполнения формы: Имя \nПол (Можно указать: Мужчина, Женщина, Не указано")
+    println("Формат заполнения формы: Имя \nПол (Укажите: мужчина или женщина \nВводите по 1 человек")
     do {
 
         val name = readln()
         val sex = readln()
-        when (sex) {
+        when (sex.lowercase()) {
             "мужчина" -> list1.add(User4(name, Sex.MAN))
             "женщина" -> list1.add(User4(name, Sex.WOMAN))
-            "не указано" -> list1.add(User4(name, Sex.NOT_SPECIFIED))
             else -> {
                 println("Введите правильно пол!")
                 continue
             }
         }
 
-        if (list1.size == 5) {
-            list1.forEach { println("Имя: ${it.name} Пол: ${it.sex.getSexString()}") }
-            break
-        }
     } while (list1.size < 5)
+
+    list1.forEach { println("Имя: ${it.name} Пол: ${it.sex.getSexString()}") }
 }
