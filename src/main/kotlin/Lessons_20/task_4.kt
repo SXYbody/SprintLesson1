fa73557d834bb22462215d14d7d822e1020af68f
saveliy <saveliykislic88@mail.ru>
2025-07-25 21:31:12 +0700

    KS-20-4 smth

--- src/main/kotlin/Lessons_20/task_4.kt
+++ src/main/kotlin/Lessons_20/task_4.kt
@@ -1,11 +1,11 @@
 package org.example.Lessons_20
 
-fun printStringLambda(expression: String) {
-    println({ "Вы нажали на елемент $expression" })
+fun printStringLambda(expression: String): String {
+    return { "Вы нажали на элемент $expression" }()
 }
 
 fun main() {
     val list1 = listOf("Гвоздь", "Доска", "Рыба", "Носок", "Арбузная семечка")
 
-    list1.map { if (list1.indexOf(it) % 2 == 0) printStringLambda(it) else println(it) }
+    list1.map { if (list1.indexOf(it) % 2 == 0) println(printStringLambda(it)) else println(it) }
 }
