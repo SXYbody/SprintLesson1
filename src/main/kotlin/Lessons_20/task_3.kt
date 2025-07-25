a3633b83b4b13d9e2bfd2f4aa35c6024e1fceb0d
saveliy <saveliykislic88@mail.ru>
2025-07-25 19:23:49 +0700

    KS-20-3

--- src/main/kotlin/Lessons_20/task_3.kt
+++ src/main/kotlin/Lessons_20/task_3.kt
@@ -1,18 +1,18 @@
 package org.example.Lessons_20
 
-class Player(
+class Player1(
     val name: String,
     val isKey: Boolean,
 )
 
 fun main() {
-    val openDoorLambda: (Player) -> String = { player: Player ->
+    val openDoorLambda: (Player1) -> String = { player: Player1 ->
         if (player.isKey) "${player.name}, вы открыли дверь!" else "Похоже ключа нету.."
     }
 
-    val player1 = Player(name = "NinjaGame11223", isKey = true)
+    val player1 = Player1(name = "NinjaGame11223", isKey = true)
     println(openDoorLambda(player1))
 
-    val player2 = Player(name = "Gamer", isKey = false)
+    val player2 = Player1(name = "Gamer", isKey = false)
     println(openDoorLambda(player2))
 }
