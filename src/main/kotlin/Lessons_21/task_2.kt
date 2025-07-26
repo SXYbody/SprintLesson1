10d0d28e8b4b7a9260ce18c6f31269a6278b4fa5
saveliy <saveliykislic88@mail.ru>
2025-07-26 21:39:11 +0700

    KS-21-2 smth

--- src/main/kotlin/Lessons_21/task_2.kt
+++ src/main/kotlin/Lessons_21/task_2.kt
@@ -1,13 +1,10 @@
 package org.example.Lessons_21
 
 fun main() {
-    val list = listOf<Int>(10, 20, 15, 63, 24)
+    val list = listOf(10, 20, 15, 63, 24)
     println(list.evenNumbersSum())
 }
 
 fun List<Int>.evenNumbersSum(): Int {
-    val list = this
-    var count = 0
-    list.forEach { if (it % 2 == 0) count += it }
-    return count
+    return this.filter { it % 2 == 0 }.sum()
 }
