06cc6207542a881f4ab8026ff7b343ff6b43ee84
saveliy <saveliykislic88@mail.ru>
2025-07-28 22:37:25 +0700

    KS-22-1 smth

--- src/main/kotlin/Lessons_22/task_1.kt
+++ src/main/kotlin/Lessons_22/task_1.kt
@@ -1,21 +1,21 @@
 package org.example.Lessons_22
 
-data class  DataBook(
+data class DataBook(
     val title: String,
     val author: String,
 )
 
-class  RegularBook(
+class RegularBook(
     val title: String,
     val author: String,
 )
 
 fun main() {
-    val dataBook1 = DataBook("Частушки","Народ")
-    val dataBook2 = DataBook("Частушки","Народ")
+    val dataBook1 = DataBook("Частушки", "Народ")
+    val dataBook2 = DataBook("Частушки", "Народ")
 
-    val regularBook1 = RegularBook("Гарри Поттер","Роулинг")
-    val regularBook2 = RegularBook("Гарри Поттер","Роулинг")
+    val regularBook1 = RegularBook("Гарри Поттер", "Роулинг")
+    val regularBook2 = RegularBook("Гарри Поттер", "Роулинг")
 
     println(dataBook1 == dataBook2) // т.к data class у нас сравниваются значения
     println(regularBook1 == regularBook2) // тут у нас сравниваются сыллки на объекты, и они в любом случае будут разные
