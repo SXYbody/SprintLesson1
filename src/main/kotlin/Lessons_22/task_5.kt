d7943be92418cbbdd73635e59ca4a82c480c3bfb
saveliy <saveliykislic88@mail.ru>
2025-07-31 21:43:26 +0700

    KS-22-5 smth

--- src/main/kotlin/Lessons_22/task_5.kt
+++ src/main/kotlin/Lessons_22/task_5.kt
@@ -5,13 +5,10 @@ data class GalacticGuide(
     val descriptor: String,
     val time: String,
     val distanceToEarth: String,
-){
-    override fun toString(): String {
-        return "Место: $event \nОписание: $descriptor \nВремя: $time \nВремя до Земли: $distanceToEarth световых лет"
-    }
-}
+)
 
 fun main() {
     val galacticGuide1 = GalacticGuide("Alpha Centauri", "Взрыв карликовой звезды", "17:05", "4")
-    println(galacticGuide1)
+    println("Событие: ${galacticGuide1.component1()} \nОписание: ${galacticGuide1.component2()} " +
+            "\nВремя: ${galacticGuide1.component3()} \nДистанция до Земли: ${galacticGuide1.component4()} световых лет" )
 }
